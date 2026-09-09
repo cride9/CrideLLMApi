5e9b900f7d34788d9c47beb877461426651915c1
cride <gamingbalint80@gmail.com>
2026-09-09 11:52:32 +0200

    Update ContextManager.cs

--- Api/ContextManager.cs
+++ Api/ContextManager.cs
@@ -29,6 +29,85 @@ public class ContextManager
     }
 
     /// <summary>
+    /// Adds a chat message to the chat history.
+    /// </summary>
+    /// <param name="role">The role of the chat message to add.</param>
+    /// <param name="content">The content of the chat message to add.</param>
+    public void Add(REQUEST_ROLE role, string content)
+    {
+        _chatHistory.Add(new ChatMessage
+        {
+            Role = role.ToString().ToLower(),
+            Content = content
+        });
+    }
+
+    /// <summary>
+    /// Adds a system message to the chat history.
+    /// </summary>
+    /// <param name="content">The content of the system message to add.</param>
+    public void AddSystem(string content)
+    {
+        Add(REQUEST_ROLE.SYSTEM, content);
+    }
+
+    /// <summary>
+    /// Adds a user message to the chat history.
+    /// </summary>
+    /// <param name="content">The content of the user message to add.</param>
+    public void AddUser(string content)
+    {
+        Add(REQUEST_ROLE.USER, content);
+    }
+
+    /// <summary>
+    /// Adds an assistant message to the chat history.
+    /// </summary>
+    /// <param name="content">The content of the assistant message to add.</param>
+    public void AddAssistant(string content)
+    {
+        Add(REQUEST_ROLE.ASSISTANT, content);
+    }
+
+    /// <summary>
+    /// Adds a tool message to the chat history with the specified tool call ID and content.
+    /// </summary>
+    /// <param name="toolCallId">The ID of the tool call.</param>
+    /// <param name="content">The content of the tool message to add.</param>
+    public void AddTool(
+        string toolCallId,
+        string content)
+    {
+        _chatHistory.Add(new ChatMessage
+        {
+            Role = REQUEST_ROLE.TOOL.ToString().ToLower(),
+            ToolCallId = toolCallId,
+            Content = content
+        });
+    }
+
+    /// <summary>
+    /// Sets the system message in the chat history, replacing any existing system messages.
+    /// </summary>
+    /// <param name="content">The content of the system message to set.</param>
+    public void SetSystem(string content)
+    {
+        _chatHistory.RemoveAll(
+            x => x.Role == REQUEST_ROLE.SYSTEM
+                .ToString()
+                .ToLower());
+
+        _chatHistory.Insert(0, new ChatMessage
+        {
+            Role = REQUEST_ROLE.SYSTEM
+                .ToString()
+                .ToLower(),
+
+            Content = content
+        });
+    }
+
+    /// <summary>
     /// Gets the chat messages filtered by the specified role.
     /// </summary>
     /// <param name="role">The role to filter by.</param>
